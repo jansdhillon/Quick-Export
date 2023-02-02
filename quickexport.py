1f00bb4c38d2aa32c97c19facb54951e379bc9cd
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2023-02-01 20:23:25 -0800

    I think I'm done, modified the code to preserve the copy of this program in Quick Export

--- quickexport.py
+++ quickexport.py
@@ -8,6 +8,9 @@ files = glob.glob(os.path.join(folder, '*'))
 #copy files to OneDrive folder and delete from quick export folder
 while len(files) > 0:
     file = files.pop()
-    if os.path.isfile(file) and file != 'quickexport.py':
+    print(file)
+    name = os.path.basename(file)
+    print(name)
+    if (os.path.isfile(file)) and (name != 'quickexport.py'):
         shutil.copy(file, os.path.join(os.path.join(os.path.expanduser('~')), 'OneDrive - UBC/Resumes and CL (OneDrive)'))
         os.remove(file)
